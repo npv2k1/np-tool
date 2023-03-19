6a270b4eeb66df6d25c5f43c206daa73277e8241
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-03-19 10:25:14 +0700

    hardhat: update project

--- templates/hardhat/hardhat.config.ts
+++ templates/hardhat/hardhat.config.ts
@@ -3,6 +3,36 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    development: {
+      url: "http://127.0.0.1:7545",
+      accounts: [
+        "d93e625372d02744e863fda06ffdf69ff6844d0af62ca956db73a45f5bcf416b",
+      ],
+    },
+    // testnet: {
+    //   url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+    //   chainId: 97,
+    //   gasPrice: 20000000000,
+    //   accounts: { mnemonic: mnemonic },
+    // },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  typechain: {
+    // outDir: "src/types",
+    target: "ethers-v5",
+  },
 };
 
 export default config;
